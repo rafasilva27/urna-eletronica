414a3e7a3cd2e83fcba78480ab4da5839cfb9eb3
resoftsolutions <re.softsolutions@gmail.com>
2022-01-24 18:58:53 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'avatar1.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano',
+                partido: 'DEFG',
+                fotos: [
+                    {url: 'avatar2.png', legenda: 'Vereador'}
+                ]    
+            },
+         ]
+    },
+    
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '54',
+                nome: 'Marcos',
+                partido: 'PPP',
+                vice: 'Sebastião',
+                fotos: [
+                    {url: 'avatar3.png', legenda: 'Prefeito'},
+                    {url: 'avatar1.png', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '55',
+                nome: 'Maria',
+                partido: 'PLL',
+                vice: 'Martin',
+                fotos: [
+                    {url: 'Mavatar5.png', legenda: 'Prefeito'},
+                    {url: 'avatar4.png', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+        ]
+    }
+]
